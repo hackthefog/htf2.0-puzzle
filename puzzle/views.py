4eb47c5e9044b459e4d9d3da53b4c126d82919ca
Savagecoder77 <savagecoder77@gmail.com>
2020-01-15 07:59:44 -0800

    remove extra routes

--- puzzle/views.py
+++ puzzle/views.py
@@ -3,10 +3,6 @@ from flask import render_template, redirect, url_for
 
 # Views
 @app.route("/", methods=['GET'])
-@app.route("/index", methods=['GET'])
-@app.route("/index/", methods=['GET'])
-@app.route("/home", methods=['GET'])
-@app.route("/home/", methods=['GET'])
 def index():
     return render_template('home.html')
 
