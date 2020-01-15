90b42518fd04e74d44b9bcd1244fe81501f6f4c2
Savagecoder77 <savagecoder77@gmail.com>
2020-01-15 13:41:57 -0800

    add new routes for new pages

--- puzzle/views.py
+++ puzzle/views.py
@@ -6,6 +6,14 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('home.html')
 
+@app.route("/start", methods=['GET'])
+def start():
+    return render_template('start.html')
+
+@app.route("/bot_slack?", methods=['GET'])
+def slack():
+    return render_template('slackBot.html')
+
 # Error Handelers
 @app.errorhandler(404)
 def page_not_found(e):
