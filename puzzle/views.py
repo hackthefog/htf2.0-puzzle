3135e4f0f6bc7d5efe0f6fd29363680cb1544eda
Savagecoder77 <savagecoder77@gmail.com>
2020-01-16 13:03:17 -0800

    new routes for hashing

--- puzzle/views.py
+++ puzzle/views.py
@@ -15,9 +15,16 @@ def start():
 def slack():
     return render_template('slack.html')
 
-@app.route("/hash/<input>", methods=['GET'])
-def hashing():
-    return render_template('hash.html')
+@app.route("/hash/<inputForHash>", methods=['GET'])
+def hashing(inputForHash):
+    if inputForHash.lower() == 'sha256':
+        return render_template('hash.html')
+
+@app.route("/hash/sha256/<inputForHash>", methods=['GET'])
+def hashed(inputForHash):
+    # Solution is 'c8b2d3d370a696b3e473f85dca4437264ad6d2094c1a2e2329040bbb71086abe'
+    if inputForHash.lower() == hashlib.sha256('Rafael Cenzano'.encode('utf-8')).hexdigest():
+        return render_template('hash.html')
 
 # Error Handelers
 @app.errorhandler(404)
