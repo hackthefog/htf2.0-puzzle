8a72cd99631fae7e3fcee2b7977664fe46d367f8
Savagecoder77 <savagecoder77@gmail.com>
2020-01-16 13:18:34 -0800

    update routes and cleanup

--- puzzle/views.py
+++ puzzle/views.py
@@ -3,28 +3,28 @@ from hashlib import sha256
 from flask import render_template, redirect, url_for
 
 # Views
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('home.html')
 
-@app.route("/start", methods=['GET'])
+@app.route('/start', methods=['GET'])
 def start():
     return render_template('start.html')
 
-@app.route("/bot-slack", methods=['GET'])
+@app.route('/bot-slack', methods=['GET'])
 def slack():
     return render_template('slack.html')
 
-@app.route("/hash/256", methods=['GET'])
-@app.route("/hash/256/", methods=['GET'])
-def hashing(inputForHash):
+@app.route('/found/sha256', methods=['GET'])
+def shanum():
     return render_template('hash.html')
 
-@app.route("/hash/sha256/<inputForHash>", methods=['GET'])
-def hashed(inputForHash):
+@app.route('/hash/sha256/<inputForHash>', methods=['GET'])
+@app.route('/hash/SHA256/<inputForHash>', methods=['GET'])
+def hash(inputForHash):
     # Solution is 'c8b2d3d370a696b3e473f85dca4437264ad6d2094c1a2e2329040bbb71086abe'
-    if inputForHash.lower() == hashlib.sha256('Rafael Cenzano'.encode('utf-8')).hexdigest():
-        return render_template('hash.html')
+    if inputForHash.lower() == sha256('Rafael Cenzano'.encode('utf-8')).hexdigest():
+        return '<h1>Hello There</h1>'
 
 # Error Handelers
 @app.errorhandler(404)
